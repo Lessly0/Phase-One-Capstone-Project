3d0820d35e4804f0a819b9bfc10ec8602d14a018
Lessly0 <sagelessly1@gmail.com>
2026-05-27 15:16:17 +0200

    Added dom for adding and removing books

--- js/app.js
+++ js/app.js
@@ -5,15 +5,14 @@ import {
 } from "./favorites.js";
 
 const booksgrid= document.getElementById('books-grid');
-const favorites= document.getElementById('favorites-grid');
+const favoritesgrid= document.getElementById('favorites-grid');
 
 const booksData = [
     { id: 1, title: 'The Great Gatsby', author: 'F. Scott Fitzgerald'},
     { id: 2, title: 'To Kill a Mockingbird', author: 'Harper Lee'},
     { id: 3, title: '1984',author: 'George Orwell'},
-    { id: 3, title: '1984',author: 'George Orwell'},
-    { id: 3, title: '1984',author: 'George Orwell'},
-    { id: 4, title: '1984',author: 'George Orwell'}
+    { id: 4, title: '1985',author: 'Georges Orwelln'},
+
 ];
 
 function showBooks(){
@@ -32,5 +31,51 @@ function showBooks(){
         `
     });
 
-}
+};
+
+document.addEventListener("click", (event) => {
+    if (event.target.dataset.id) {
+        const bookId = Number(event.target.dataset.id);
+        const selectedBook = booksData.find(
+            book => book.id === bookId
+        );
+
+        addFavorite(selectedBook);
+        renderFavorites();
+    }
+});
+
+function showFavorites(){
+    const favoritesData = getFavorites();
+    favoritesgrid.innerHTML = "";
+
+    favoritesData.forEach(book => {
+        favoritesgrid.innerHTML += `
+            <div class="bg-white p-4 rounded-xl shadow-xl text-black">
+                <h3 class="font-bold">${book.title}</h3>
+                <p>By: ${book.author}</p>
+
+                <button
+                    class="mt-2 bg-red-600 text-white px-3 py-2 rounded-xl"
+                    data-remove="${book.id}">
+                    Remove
+                </button>
+            </div>
+        `;
+    });
+};
+
+document.addEventListener("click", (event) => {
+
+    if (event.target.dataset.remove) {
+
+        const bookId = Number(event.target.dataset.remove);
+
+        removeFavorite(bookId);
+
+        renderFavorites();
+    }
+});
+
 showBooks();
+showFavorites();
